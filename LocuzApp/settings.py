d6c952b9f9c96cdcf0d523a61356169f01f356e8
mdaslam <mdaslam4657@gmail.com>
2024-07-24 11:28:26 +0530

    aaded hists

--- LocuzApp/settings.py
+++ LocuzApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xxt6abpabl64^2u8_s8wi+ek=7%al3i#ywqw+tn-w8-4st&aq$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ab67795e29e084da2a1f9e93e6308b1c-1218764334.ap-south-1.elb.amazonaws.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
